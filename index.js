8ee094cc9306b4c1b85d5be13629e82df7b37572
Claire Hendry <claire.d.hendry@gmail.com>
2020-02-20 09:19:40 -0500

    updates

--- index.js
+++ index.js
@@ -1,46 +1,64 @@
 'use strict'
 
-function getRandomDogs(number) {
-   const value = $("input#input-number").val();
-   
-    if ((number <= 50) && (number > 0)) {
-    const fetchNumber = `https://dog.ceo/api/breeds/image/random/${number}`
-    fetch(fetchNumber)
+
+//check if breed is in list
+
+function verifyBreed(breed) {
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(response => response.json())
+    .then(responseJson => checkForBreed(responseJson, breed))  
+}
+
+function checkForBreed(responseJson, breed) {
+    const breedArray = Object.keys(responseJson.message);
+    const found = breedArray.find(element => element === breed);
+    if (found === breed) {
+        fetchDogBreedImage(breed);
+    } else {
+        alert("dog breed not found");
+    }    
+}
+
+
+function fetchDogBreedImage(breed) {
+    const fetchBreed = `https://dog.ceo/api/breed/${breed}/images`;
+    fetch(fetchBreed)
     .then(response => response.json())
     .then(responseJson => displayDogs(responseJson))
-    .catch(error => alert("something's not right."))
-    
-}  else { 
-    fetch('https://dog.ceo/api/breeds/image/random/3')
-    .then(response => response.json())
-    .then(responseJson => displayDogs(responseJson))
-    .catch(error => alert("something's not right."))
 }
-}
+
+
+
+
+//display dog img in DOM
 
 function displayDogs(responseJson) {
    $("dog-images, img").remove();
    console.log(responseJson.message);
    $(".dog-images").removeClass("hidden");
-   for (let i = 0; i < responseJson.message.length; i++) {
+   
 
-   let dogImage = generateDogImage(responseJson, i);
+   let dogImage = generateDogImage(responseJson);
   
    $(".dog-images").append(dogImage);
-   }
+   
    
 }
 
-function generateDogImage(responseJson, i) {
-   return `<img src="${responseJson.message[i]}" class="results-image"/>`
+function generateDogImage(responseJson) {
+    let number = Math.floor((Math.random() * responseJson.message.length) + 1);
+
+   return `<img src="${responseJson.message[number]}" class="results-image"/>`
 }
 
+//get input value
 
-function getNumber() {
+function getValue() {
    $("#submit").on("click", function(event) {
        event.preventDefault();
        const value = $("input#input-number").val();
-       getRandomDogs(value);
+
+       verifyBreed(value);
        
    })   
 }
@@ -48,8 +66,4 @@ function getNumber() {
 
 
 
-// $(getRandomDogs)
-$(getNumber)
-
-
-
+$(getValue);
